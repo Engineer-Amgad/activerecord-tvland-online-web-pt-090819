04514c394d2786483dc8c8841b870443ea18e3a2
Amgad Malik <amgadmalik@yahoo.com>
2019-12-23 21:21:54 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    #returns the first and last name of an actor
+   "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    # lists all of the characters that actor has alongside the show that the character is in
+
+    "#{shows.first.characters.first.name} - #{shows.first.name}"
+
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Show < ActiveRecord::Base
   
+  # has_many :actors
+  has_many :characters
+  belongs_to :network
+  
+  def actors_list
+    binding.pry
+    "#{characters.first}"
+  end 
 end
